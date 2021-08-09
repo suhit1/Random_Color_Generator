290569c29846697702113d5633b62347e4435ca7
Suhit Gupta <suhitgupta111@gmail.com>
2021-08-09 17:23:30 +0530

    Pus

---
+++ Hex.js
@@ -0,0 +1,51 @@
+// Reference of elements
+
+let click = document.getElementById("click");
+let display_background_color = document.getElementById(
+  "display_background_color"
+);
+let display_color_name = document.getElementById("display_color_name");
+let background_color = document.getElementById("background_color");
+let color_name = document.getElementById("color_name");
+
+// array
+
+let hex_array = [
+  0,
+  1,
+  2,
+  3,
+  4,
+  5,
+  6,
+  7,
+  "A",
+  "B",
+  "C",
+  "D",
+  "E",
+  "F",
+  "G",
+  "H",
+  "Z",
+];
+
+click.addEventListener("click", function () {
+  let index = Math.floor(Math.random() * hex_array.length); // generate random numbers from 0 to 9
+  let generate_color_name = "#";
+
+  for (let i = 0; i < 2; i++) {
+    generate_color_name += hex_array[index];
+  }
+
+  if (index + 2 < hex_array.length) {
+    for (let i = 0; i < 2; i++) {
+      generate_color_name += hex_array[index + 2];
+      generate_color_name += hex_array[index + 1];
+    }
+  }
+
+  color_name.innerText = generate_color_name;
+  document.body.style.backgroundColor = generate_color_name;
+  console.log(generate_color_name);
+});
---
+++ script.js
@@ -0,0 +1,39 @@
+// Reference of elements
+
+let click = document.getElementById("click");
+let display_background_color = document.getElementById(
+  "display_background_color"
+);
+let display_color_name = document.getElementById("display_color_name");
+let background_color = document.getElementById("background_color");
+let color_name = document.getElementById("color_name");
+
+// Arrays
+
+let color_flipper = [
+  "red",
+  "green",
+  "blue",
+  "orange",
+  "grey",
+  "lime",
+  "yellow",
+  "pink",
+  "gold",
+  "darkblue",
+  "seagreen",
+  "white",
+  "brown",
+  "skyblue",
+  "violet",
+  "darkgreen",
+];
+
+// Event Listeners
+
+click.addEventListener("click", function () {
+  let index = Math.floor(Math.random() * 16); // generate random numbers from 0 to 9
+  document.body.style.backgroundColor = `  ${color_flipper[index]}`;
+
+  color_name.innerText = color_flipper[index];
+});
